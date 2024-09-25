25baacb5e3d99a57b8128be6181d010bc698781e
Suhendi Andigo <suhendiandigo@gmail.com>
2024-09-26 00:32:17 +0700

    feat: Add support for pydantic-settings

---
+++ composify/pydantic_settings/__init__.py
@@ -0,0 +1,3 @@
+from .settings_provider import PydanticSettingsProvider
+
+__all__ = ("PydanticSettingsProvider",)
---
+++ tests/integration/test_pydantic_settings.py
@@ -0,0 +1,17 @@
+from pydantic_settings import BaseSettings
+
+from composify.applications import Composify
+from composify.pydantic_settings import PydanticSettingsProvider
+
+
+class ExampleSettings(BaseSettings, frozen=True):
+    value: int = 5
+
+
+def test_request_setting():
+    composify = Composify(providers=[PydanticSettingsProvider()])
+
+    settings = composify.get_or_create(ExampleSettings)
+
+    assert isinstance(settings, ExampleSettings)
+    assert settings.value == 5
