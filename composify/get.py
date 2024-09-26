2acd520b78be858af7ac2852195ab9f176be9845
Suhendi Andigo <suhendiandigo@gmail.com>
2024-09-26 14:13:21 +0700

    feat: Add maybe_one to Get

--- composify/get.py
+++ composify/get.py
@@ -4,6 +4,7 @@ from abc import ABC, abstractmethod
 from collections.abc import Sequence
 from typing import TypeVar
 
+from composify.errors import InstanceOfTypeNotFoundError
 from composify.types import AnnotatedType
 
 T = TypeVar("T")
@@ -13,6 +14,23 @@ class Get(ABC):
     """Getter protocol. This protocols allows for retrieval of instances that
     has previously been created via Builder or AsyncBuilder.
     """
+
+    def maybe_one(
+        self,
+        type_: AnnotatedType[T],
+    ) -> T | None:
+        """Get an object of type.
+
+        Args:
+            type_ (AnnotatedType[T]): The type of the object to retrieve.
+
+        Returns:
+            T | None: An existing instance of T; otherwise None.
+        """
+        try:
+            return self.one(type_)
+        except InstanceOfTypeNotFoundError:
+            return None
 
     @abstractmethod
     def one(
@@ -26,6 +44,9 @@ class Get(ABC):
 
         Returns:
             T: An existing instance of T.
+
+        Raises:
+            InstanceOfTypeNotFoundError: If an instance do not exists.
         """
         raise NotImplementedError()
 
